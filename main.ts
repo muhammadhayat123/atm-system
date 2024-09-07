50dbabbcabe3bf37941dbcf78e14f4f127ec34c1
hayatmyhammad <chartard12@gmail.com>
2024-09-07 22:48:20 +0500

    changes

---
+++ main.ts
@@ -0,0 +1,53 @@
+#! usr/bin/env node 
+import inquirer from "inquirer";
+
+let myBalance = 10000;
+let myPin = 1234;
+
+    let pinAnswer = await inquirer.prompt([
+        {
+            name: "pin",
+            message: "Enter your pin",
+            type: "number"
+        }
+    ]);
+
+    if (pinAnswer.pin === myPin) {
+        console.log("Correct pin code!");
+
+        let operationAns = await inquirer.prompt([
+            {
+                name: "operation",
+                message: "Please select an option",
+                type: "list",
+                choices: ["withdraw", "check balance"]
+            }
+        ]);
+
+        if (operationAns.operation === "withdraw") {
+            let amountAns = await inquirer.prompt([
+                {
+                    name: "amount",
+                    message: "Enter the withdrawal amount",
+                    type: "number"
+                }
+            ]);
+
+            if (amountAns.amount > myBalance) {
+                console.log("Insufficient funds!");
+           
+            } else {
+                myBalance -= amountAns.amount;
+                console.log("Withdrawal successful!");
+                console.log("Your remaining balance is: " + myBalance);
+            }
+        } else if (operationAns.operation === "check balance") {
+            console.log("Your balance is: " + myBalance);
+        }
+    } else {
+        console.log("Incorrect pin.");
+    }
+
+
+
+
